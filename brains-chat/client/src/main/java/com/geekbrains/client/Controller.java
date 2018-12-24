349dc248ab54bf1b6f767922eb548bc30501a405
garybmx <gary.ukolov@gmail.com>
2018-12-24 16:55:48 +0300

    дз

---
+++ brains-chat/client/src/main/java/com/geekbrains/client/Controller.java
@@ -0,0 +1,133 @@
+package com.geekbrains.client;
+
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField msgField, loginField;
+
+    @FXML
+    HBox msgPanel, authPanel;
+
+    @FXML
+    PasswordField passField;
+
+    @FXML
+    ListView<String> clientsList;
+
+    private boolean authentificated;
+    private String nickname;
+
+    public void setAuthentificated(boolean authentificated) {
+        this.authentificated = authentificated;
+        authPanel.setVisible(!authentificated);
+        authPanel.setManaged(!authentificated);
+        msgPanel.setVisible(authentificated);
+        msgPanel.setManaged(authentificated);
+        clientsList.setVisible(authentificated);
+        clientsList.setManaged(authentificated);
+        if (!authentificated) {
+            nickname = "";
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setAuthentificated(false);
+        clientsList.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2) {
+                String nickname = clientsList.getSelectionModel().getSelectedItem();
+                msgField.setText("/w " + nickname + " ");
+                msgField.requestFocus();
+                msgField.selectEnd();
+            }
+        });
+        linkCallbacks();
+    }
+
+    public void sendAuth() {
+        Network.sendAuth(loginField.getText(), passField.getText(), this);
+        loginField.clear();
+        passField.clear();
+    }
+
+    public void sendMsg() {
+        if (Network.sendMsg(msgField.getText())) {
+            msgField.clear();
+            msgField.requestFocus();
+        }
+    }
+
+    public void showAlert(String msg) {
+        Platform.runLater(() -> {
+            Stage dialogStage = new Stage();
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            Button ok = new Button("Пнятно");
+            VBox vbox = new VBox(new Text(msg), ok);
+            vbox.setAlignment(Pos.CENTER);
+            vbox.setPadding(new Insets(15));
+
+            ok.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                   dialogStage.close();
+                }
+            });
+
+            dialogStage.setScene(new Scene(vbox));
+            dialogStage.show();
+        });
+    }
+
+    public void linkCallbacks() {
+
+        Network.setCallOnException(args -> showAlert(args[0].toString()));
+
+        Network.setCallOnCloseConnection(args ->Platform.runLater(() -> setAuthentificated(false)));
+
+        Network.setCallOnAuthentificated(args -> {
+            Platform.runLater(() -> setAuthentificated(true));
+            nickname = args[0].toString();
+        });
+
+        Network.setCallOnMsgReceived(args -> {
+            String msg = args[0].toString();
+            if (msg.startsWith("/")) {
+                if (msg.startsWith("/clients ")) {
+                    String[] tokens = msg.split("\\s");
+                    Platform.runLater(() -> {
+                        clientsList.getItems().clear();
+                        for (int i = 1; i < tokens.length; i++) {
+                            clientsList.getItems().add(tokens[i]);
+                        }
+                    });
+                }
+            } else {
+                Platform.runLater(() -> textArea.appendText(msg + "\n"));
+            }
+        });
+    }
+}
